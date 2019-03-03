8a6a39080767dd488013c16150becd24fffcbfaa
Patrick A <akolo@hotmail.com>
2019-03-03 19:46:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,14 +14,14 @@ def using_reverse(array)
   array.reverse 
 end
 
-def using_first(array)
-
+def using_first("lil' bub", "grumpy cat", "maru")
+    using_firt.first 
 end
 
-def using_last(array)
-
+def using_last("lil' bub", "grumpy cat", "maru")
+    using_last.last 
 end
 
-def using_size(array)
-
+def using_size("lil' bub", "grumpy cat", "maru")
+    using_size.size
 end
